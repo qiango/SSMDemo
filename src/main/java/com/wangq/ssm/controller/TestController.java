88213fd845305a4df5223b4f768c3dc947c2f4ab
wangqian <13437194372@163.com>
2017-10-11 17:59:09 +0800

    temp

--- src/main/java/com/wangq/ssm/controller/TestController.java
+++ src/main/java/com/wangq/ssm/controller/TestController.java
@@ -1,26 +1,32 @@
 package com.wangq.ssm.controller;
 
-import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.wangq.ssm.entity.Test;
 import com.wangq.ssm.service.TestService;
 import com.wangq.ssm.util.ChanngeUtil;
 import com.wangq.ssm.util.ValueUtil;
 import org.apache.http.HttpStatus;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletRequest;
+import java.io.*;
 import java.util.Map;
 
 
 @Controller
 @RequestMapping("/test")
 public class TestController {
+
+    private static Logger logger = Logger.getLogger(TestMain.class);
 
     @Autowired
     private TestService serviceUser;
@@ -67,5 +73,77 @@ public class TestController {
     public Object find2(Integer id){
         return ChanngeUtil.toJson(HttpStatus.SC_OK,serviceUser.deleteUser(id));
     }
+
+    @ResponseBody
+    @RequestMapping(value="/8",method = RequestMethod.GET)
+    public Object find3(ServletRequest request){
+        HttpServletRequest httpRequest = (HttpServletRequest)request;
+        String partlyUrl = httpRequest.getRequestURI();
+        String method = httpRequest.getMethod();
+        logger.info("请求路径为==》"+partlyUrl +"  "+method);
+        JSONArray jsonArray=new JSONArray();
+        for(int i=0;i<3;i++) {
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("aa", "aaaa");
+            jsonObject.put("bb", "bbbb");
+            jsonObject.put("cc", "cccc");
+            jsonArray.add(jsonObject);
+            logger.info(jsonObject);
+        }
+        return ChanngeUtil.toJson(HttpStatus.SC_OK,jsonArray);
+    }
+    //文件上传
+    @ResponseBody
+    @RequestMapping(value="/9",method = RequestMethod.POST)
+    public Object find4(@RequestParam("uploadFiles")MultipartFile uploadFiles){
+        String tempPath="/home/hz/apps/wangqian";
+        File tmpFile = new File(tempPath);
+        if (!tmpFile.exists()) {
+            //创建临时目录
+            tmpFile.mkdir();
+        }
+        String origName = uploadFiles.getOriginalFilename();
+        InputStream inputStream = null;
+        try {
+            //创建一个文件输出流
+            FileOutputStream out = new FileOutputStream("/home/hz/apps/wangqian/"+origName);
+            inputStream = uploadFiles.getInputStream();
+            byte[] bytes = new byte[1024];
+            int len = 0;
+            while ((len = inputStream.read(bytes)) != -1)
+            {
+                out.write(bytes, 0, len);
+            }
+            inputStream.close();
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return ChanngeUtil.toJson(HttpStatus.SC_OK);
+    }
+
+    @ResponseBody
+    @RequestMapping(value="/10",method = RequestMethod.POST)
+    public Object find5(@RequestParam("uploadFiles")MultipartFile uploadFiles){
+        String tempPath="/home/hz/apps/wangqian";
+        File tmpFile = new File(tempPath);
+        if (!tmpFile.exists()) {
+            //创建临时目录
+            tmpFile.mkdir();
+        }
+        String origName = uploadFiles.getOriginalFilename();
+        try {
+            //创建一个文件输出流
+            byte[] bytes = uploadFiles.getBytes();
+            FileOutputStream out = new FileOutputStream("/home/hz/apps/wangqian/"+origName);
+            out.write(bytes);
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return ChanngeUtil.toJson(HttpStatus.SC_OK);
+    }
+
+
 
 }
--- src/main/java/com/wangq/ssm/controller/TestMain.java
+++ src/main/java/com/wangq/ssm/controller/TestMain.java
@@ -13,6 +13,14 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @ContextConfiguration(locations = {"classpath:spring-mybatis.xml"})
 public class TestMain {
     private static Logger logger = Logger.getLogger(TestMain.class);
-        //  private ApplicationContext ac = null;
 
+    public static void main(String[] args) throws Exception {
+        // 记录debug级别的信息
+        logger.debug("This is debug message.");
+        // 记录info级别的信息
+        logger.info("This is info message.");
+        // 记录error级别的信息
+        logger.error("This is error message.");
     }
+
+}
